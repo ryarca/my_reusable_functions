7758cc3a7a67628eb77c25016a1f5862d34384e9
Arca Mariangela <mariangela.arca@ext.ema.europa.eu>
2023-04-24 18:25:13 +0200

    Make the code run much faster

--- R/find_string_in_r_files.R
+++ R/find_string_in_r_files.R
@@ -1,27 +1,32 @@
-# This code searches for string(use it for function, R objects, params or other) 
-# target_object in all R files within the working directory
+library(readr)
 
 # Define the target string name
-target_string <- "store_text_input"
+target_string <- "import_test"
 
-# List all R files in the Shiny app directory and its subdirectories
+# List all R and Python files in the directory and its subdirectories
 r_files <- list.files(path = ".", pattern = "\\.R|py$", recursive = TRUE)
 
-# Initialize a variable to track whether the object is found
-string_found <- FALSE
-
-# Iterate over each R file and search for the target string
-for (file in r_files) {
-  file_content <- readLines(file)
+# Define a function to search for the target string in a file
+search_string <- function(file, target_string) {
+  file_content <- readr::read_lines(file, skip_empty_rows = TRUE)
+  
   string_indices <- grep(target_string, file_content)
   
   if (length(string_indices) > 0) {
     cat("String", target_string, "found in", file, "at lines:", string_indices, "\n")
-    string_found <- TRUE
+    return(TRUE)
   }
+  return(FALSE)
 }
+
+# Use lapply to search for the target string in each R and Python file
+string_found_list <- lapply(r_files, search_string, target_string = target_string)
+
+# Check if the string was found in any file
+string_found <- any(unlist(string_found_list))
 
 # Print a message if the string is not found
 if (!string_found) {
-  cat("String", target_string, "not found in any R files.\n")
+  cat("String", target_string, "not found in any R or Python files.\n")
 }
+
